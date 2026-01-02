2349b8d662c4e2914f0d063dc2a801162c12bd3a
Ghost <duchojakub987@gmail.com>
2026-01-02 12:05:44 +0100

    Hello, world in raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use termion::{raw::IntoRawMode, clear, cursor}; 
+use std::io::{self, Write};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = io::stdout().into_raw_mode().unwrap();
+    write!(stdout, "{}",  clear::All).unwrap();
+    write!(stdout, "{}", cursor::Goto(10, 10)).unwrap();
+    write!(stdout, "Hello, World!").unwrap();
+    let _ = stdout.flush();
+    thread::sleep(Duration::from_secs(5));
 }
